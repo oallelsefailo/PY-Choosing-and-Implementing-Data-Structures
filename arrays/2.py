3e5aa19e83a6ce3f150d5cbd05aaecc21a3ecd6b
Brandon <oallelsefailo@gmail.com>
2023-11-15 17:55:36 -0800

    Assignment done

---
+++ arrays/2.py
@@ -0,0 +1,7 @@
+import numpy as np
+
+num = np.array([1, 6, 3])
+
+sum = np.sum(num)
+
+print(sum)
--- arrays/3.py
+++ arrays/3.py
@@ -1,1 +1,7 @@
+import numpy as np
+num = np.array([1, 6, 3])
+
+results = num * 3
+
+print(results)
 
